84251b1af950df84bceed8ab96c7ff747c9f45bb
trumpxelonmusk <trumpxelonmusk@gmail.com>
2026-01-14 10:46:57 +0530

    Fix env loading

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-export { ZKEligibilitySDK } from './sdk/index';
-export * from './types/Rule';
-export * from './types/Proof';
-export * from './types/Provider';
-export * from './types/Errors';
+import "dotenv/config";
+
+export { ZKEligibilitySDK } from "./sdk";
+export * from "./types/Rule";
+export * from "./types/Proof";
+export * from "./types/Provider";
+export * from "./types/Errors";
+
